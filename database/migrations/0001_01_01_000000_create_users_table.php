1327fec44a938ce119e94a3e04fb1d11bf288b74
merloydjess <merloydaroco@gmail.com>
2024-06-20 19:25:23 +0800

    Fixed the .env file for mysql

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,10 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('name');
+            $table->decimal('price', 8, 2);
+            $table->text('description')->nullable();
+
             $table->timestamps();
         });
 
